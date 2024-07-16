ad617543ab75500106dc9b7e3efb33179428ddc2
webwithsumit <sumit2410kushwaha@gmail.com>
2024-07-16 22:43:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function hovering(){
+    document.querySelectorAll(".productsbox")
+        .forEach(function (e) {
+            var a;
+            e.addEventListener("mouseenter", function (dets) {
+                a = this.querySelector("button").textContent
+                this.querySelector("button").textContent = 'View Detils'
+            })
+            e.addEventListener("mouseleave", function (dets) {
+                this.querySelector("button").textContent = a;
+            })
+        })
+}
+hovering();
+
